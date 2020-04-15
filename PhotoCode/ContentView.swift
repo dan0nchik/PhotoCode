6a56563f4f3524fe19350daabf2c0ed21dd4f875
Daniel Khromov <danielkhromov@gmail.com>
2020-04-15 13:53:24 +0300

    Initial Commit

---
+++ PhotoCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PhotoCode
+//
+//  Created by Daniel Khromov on 4/15/20.
+//  Copyright © 2020 Daniel Khromov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
