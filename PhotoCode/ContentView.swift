ef3ee4f4dca7e9b16a976b79487e3d211f7454f5
Daniel Khromov <danielkhromov@gmail.com>
2020-04-15 14:18:40 +0300

    main page ready

--- PhotoCode/ContentView.swift
+++ PhotoCode/ContentView.swift
@@ -10,7 +10,103 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+        Spacer(minLength: 40)
+        Text("P H O T O C O D E")
+        .bold()
+            .font(.title)
+            
+            
+            HStack{
+            ZStack{
+        RoundedRectangle(cornerRadius: 23)
+            .foregroundColor(Color("violet"))
+            .frame(width: 150, height: 150)
+            .padding()
+                VStack{
+            Image(systemName: "person.circle.fill")
+                .foregroundColor(.white)
+                .font(.title)
+            Text("Account")
+                    .foregroundColor(.white)
+                    .font(.title)
+                    
+                }
+            }
+            ZStack{
+            RoundedRectangle(cornerRadius: 23)
+                .foregroundColor(Color("green"))
+                .frame(width: 150, height: 150)
+                .padding()
+            VStack{
+            Image(systemName: "camera.fill")
+                .foregroundColor(.white)
+                .font(.title)
+            Text("Camera")
+                    .foregroundColor(.white)
+                    .font(.title)
+                    
+                }
+            }
+        }
+            ZStack
+                {
+                RoundedRectangle(cornerRadius: 23)
+                           .foregroundColor(Color("yellow"))
+                           .frame(width: 350, height: 150)
+                           .padding()
+                HStack{
+                Image(systemName: "photo.fill.on.rectangle.fill")
+                    .foregroundColor(.white)
+                    .font(.title)
+                Text("From library")
+                        .foregroundColor(.white)
+                        .font(.title)
+                        
+                    }
+            }
+            HStack{
+                VStack {
+                    ZStack{
+                    RoundedRectangle(cornerRadius: 23)
+                               .foregroundColor(Color("red"))
+                               .frame(width: 150, height: 230)
+                               .padding()
+                    VStack{
+                    Text("GO")
+                        .foregroundColor(.white)
+                        .font(.title)
+                        .bold()
+                    Text("to your \n codes")
+                            .foregroundColor(.white)
+                            .font(.title)
+                            
+                        }
+                    }
+                    Spacer()
+                }
+                VStack{
+                ZStack{
+                 RoundedRectangle(cornerRadius: 23)
+                      .foregroundColor(Color("blue"))
+                       .frame(width: 150, height: 110)
+                       .padding()
+                    VStack{
+                    Text("12")
+                        .foregroundColor(.white)
+                        .font(.title)
+                        .bold()
+                    Text("Scanned")
+                            .foregroundColor(.white)
+                            .font(.title)
+                            
+                        }
+                }
+                Spacer()
+                    
+                }
+            }
+            
     }
 }
 
@@ -18,4 +114,5 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
 }
