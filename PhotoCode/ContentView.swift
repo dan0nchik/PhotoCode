cb58046fe5143215bb8246278701869bb5bad0e8
Daniel Khromov <danielkhromov@gmail.com>
2020-04-16 14:11:32 +0300

    recognises

--- PhotoCode/ContentView.swift
+++ PhotoCode/ContentView.swift
@@ -14,120 +14,120 @@ struct ContentView: View {
     @State var showLib = false
     var body: some View {
         NavigationView{
-        VStack{
-        
-        Text("P H O T O C O D E")
-        .bold()
-            .font(.title)
-            
-            
-            HStack{
-            ZStack{
-        RoundedRectangle(cornerRadius: 23)
-            .foregroundColor(Color("violet"))
-            .frame(width: 150, height: 150)
-            .padding()
-                VStack{
-            Image(systemName: "person.circle.fill")
-                .foregroundColor(.white)
-                .font(.title)
-            Text("Account")
-                    .foregroundColor(.white)
-                    .font(.title)
-                    
-                }
-            }
-            ZStack{
-            RoundedRectangle(cornerRadius: 23)
-                .foregroundColor(Color("green"))
-                .frame(width: 150, height: 150)
-                .padding()
-                .onTapGesture {
-                    self.showCamera.toggle()
-                    self.useCamera = true
-                }
             VStack{
-            Image(systemName: "camera.fill")
-                .foregroundColor(.white)
-                .font(.title)
                 
-                NavigationLink(destination: Scan(useCamera: $useCamera), isActive: $showCamera) {
-                    Text("Camera")
-                        .foregroundColor(.white)
-                        .font(.title)
-                }
-            
-                    
-                }
-            }
-        }
-            ZStack
-                {
-                RoundedRectangle(cornerRadius: 23)
-                           .foregroundColor(Color("yellow"))
-                           .frame(width: 350, height: 150)
-                           .padding()
-                        .onTapGesture {
-                            self.showLib.toggle()
-                            self.useCamera = false
-                        }
-                HStack{
-                Image(systemName: "photo.fill.on.rectangle.fill")
-                    .foregroundColor(.white)
+                Text("P H O T O C O D E")
+                    .bold()
                     .font(.title)
-                    
-                NavigationLink(destination: Scan(useCamera: $useCamera), isActive: $showLib) {
-                                   Text("Library")
-                                       .foregroundColor(.white)
-                                       .font(.title)
-                               }
+                
+                
+                HStack{
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 23)
+                            .foregroundColor(Color("violet"))
+                            .frame(width: 150, height: 150)
+                            .padding()
+                        VStack{
+                            Image(systemName: "person.circle.fill")
+                                .foregroundColor(.white)
+                                .font(.title)
+                            Text("Account")
+                                .foregroundColor(.white)
+                                .font(.title)
+                            
+                        }
+                    }
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 23)
+                            .foregroundColor(Color("green"))
+                            .frame(width: 150, height: 150)
+                            .padding()
+                            .onTapGesture {
+                                self.showCamera = true
+                                self.useCamera = true
+                        }
+                        VStack{
+                            Image(systemName: "camera.fill")
+                                .foregroundColor(.white)
+                                .font(.title)
+                            
+                            NavigationLink(destination: Scan(useCamera: $useCamera), isActive: $showCamera) {
+                                Text("Camera")
+                                    .foregroundColor(.white)
+                                    .font(.title)
+                            }
+                            
+                            
+                        }
+                    }
+                }
+                ZStack
+                    {
+                        RoundedRectangle(cornerRadius: 23)
+                            .foregroundColor(Color("yellow"))
+                            .frame(width: 350, height: 150)
+                            .padding()
+                            .onTapGesture {
+                                self.showLib = true
+                                self.useCamera = false
+                        }
+                        HStack{
+                            Image(systemName: "photo.fill.on.rectangle.fill")
+                                .foregroundColor(.white)
+                                .font(.title)
+                            
+                            NavigationLink(destination: Scan(useCamera: $useCamera), isActive: $showLib) {
+                                Text("Library")
+                                    .foregroundColor(.white)
+                                    .font(.title)
+                            }
+                            
+                        }
+                }
+                HStack{
+                    VStack {
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 23)
+                                .foregroundColor(Color("red"))
+                                .frame(width: 150, height: 230)
+                                .padding()
+                            VStack{
+                                Text("GO")
+                                    .foregroundColor(.white)
+                                    .font(.title)
+                                    .bold()
+                                Text("to your \n codes")
+                                    .foregroundColor(.white)
+                                    .font(.title)
+                                
+                            }
+                        }
+                        Spacer()
+                    }
+                    VStack{
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 23)
+                                .foregroundColor(Color("blue"))
+                                .frame(width: 150, height: 110)
+                                .padding()
+                            VStack{
+                                Text("12")
+                                    .foregroundColor(.white)
+                                    .font(.title)
+                                    .bold()
+                                Text("Scanned")
+                                    .foregroundColor(.white)
+                                    .font(.title)
+                                
+                            }
+                        }
+                        Spacer()
                         
                     }
+                }
+                Spacer(minLength: 100)
             }
-            HStack{
-                VStack {
-                    ZStack{
-                    RoundedRectangle(cornerRadius: 23)
-                               .foregroundColor(Color("red"))
-                               .frame(width: 150, height: 230)
-                               .padding()
-                    VStack{
-                    Text("GO")
-                        .foregroundColor(.white)
-                        .font(.title)
-                        .bold()
-                    Text("to your \n codes")
-                            .foregroundColor(.white)
-                            .font(.title)
-                            
-                        }
-                    }
-                    Spacer()
-                }
-                VStack{
-                ZStack{
-                 RoundedRectangle(cornerRadius: 23)
-                      .foregroundColor(Color("blue"))
-                       .frame(width: 150, height: 110)
-                       .padding()
-                    VStack{
-                    Text("12")
-                        .foregroundColor(.white)
-                        .font(.title)
-                        .bold()
-                    Text("Scanned")
-                            .foregroundColor(.white)
-                            .font(.title)
-                            
-                        }
-                }
-                Spacer()
-                    
-                }
-            }
-       Spacer(minLength: 100)
-    }
-}
+        }
     }
 }
 struct ContentView_Previews: PreviewProvider {
--- PhotoCode/Scan.swift
+++ PhotoCode/Scan.swift
@@ -11,24 +11,27 @@ struct Scan: View {
     @Binding var useCamera: Bool
     @State var showCaptureImageView=false
     @State var image: Image? = nil
-    
+    @State var result: String? = nil
     var body: some View {
         ZStack {
             VStack {
                 //img
                 image?.resizable()
-                  .frame(width: 250, height: 250)
+                    .frame(width: 250, height: 250)
                     .aspectRatio(contentMode: .fit)
                 Button(action: {
-                    print(0)
+                    self.result = callOCRSpace(image: self.image)!
+                    
                 }, label: {
                     Text("Scan!")
                 })
+                
+                
             }
             if (showCaptureImageView) {
-              CaptureImageView(isShown: $showCaptureImageView, image: $image, camera: $useCamera)
+                CaptureImageView(isShown: $showCaptureImageView, image: $image, camera: $useCamera)
             }
-
+            
         }.onAppear {
             self.showCaptureImageView.toggle()
             print(self.useCamera)
@@ -50,7 +53,7 @@ struct CaptureImageView {
     @Binding var camera: Bool
     
     func makeCoordinator() -> Coordinator {
-      return Coordinator(isShown: $isShown, image: $image)
+        return Coordinator(isShown: $isShown, image: $image)
     }
 }
 extension CaptureImageView: UIViewControllerRepresentable {
@@ -71,3 +74,89 @@ extension CaptureImageView: UIViewControllerRepresentable {
     }
 }
 
+func callOCRSpace(image: Image?) -> String?{
+    var final = ""
+    // Create URL request
+    let url = URL(string: "https://api.ocr.space/Parse/Image")
+    var request: URLRequest? = nil
+    if let url = url {
+        request = URLRequest(url: url)
+    }
+    request?.httpMethod = "POST"
+    let boundary = "randomString"
+    request?.addValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+    
+    let session = URLSession.shared
+    
+    // Image file and parameters
+    let imageData = UIImage(named: "test")?.jpegData(compressionQuality: 0.6)
+    let parametersDictionary = ["apikey" : "9ae1728a2c88957", "isOverlayRequired" : "True", "language" : "eng"]
+    
+    // Create multipart form body
+    let data = createBody(withBoundary: boundary, parameters: parametersDictionary, imageData: imageData, filename: "test.jpg")
+    
+    request?.httpBody = data
+    
+    // Start data session
+    var task: URLSessionDataTask? = nil
+    if let request = request {
+        task = session.dataTask(with: request, completionHandler: { (data, response, error) in
+            var result: [AnyHashable : Any]? = nil
+            do {
+                if let data = data {
+                    result = try JSONSerialization.jsonObject(with: data, options: []) as? [AnyHashable : Any]
+                }
+            } catch let myError {
+                print(myError)
+            }
+            if let res = result!["ParsedResults"] as? [[String:Any]],
+                let text = res.first{
+                let output = text["ParsedText"].unsafelyUnwrapped
+                final = output as! String
+                print(final)
+            }
+            
+        })
+    }
+    task?.resume()
+    return final
+}
+
+func createBody(withBoundary boundary: String?, parameters: [AnyHashable : Any]?, imageData data: Data?, filename: String?) -> Data? {
+    var body = Data()
+    if data != nil {
+        if let data1 = "--\(boundary ?? "")\r\n".data(using: .utf8) {
+            body.append(data1)
+        }
+        if let data1 = "Content-Disposition: form-data; name=\"\("file")\"; filename=\"\(filename ?? "")\"\r\n".data(using: .utf8) {
+            body.append(data1)
+        }
+        if let data1 = "Content-Type: image/jpeg\r\n\r\n".data(using: .utf8) {
+            body.append(data1)
+        }
+        if let data = data {
+            body.append(data)
+        }
+        if let data1 = "\r\n".data(using: .utf8) {
+            body.append(data1)
+        }
+    }
+    
+    for key in parameters!.keys {
+        if let data1 = "--\(boundary ?? "")\r\n".data(using: .utf8) {
+            body.append(data1)
+        }
+        if let data1 = "Content-Disposition: form-data; name=\"\(key)\"\r\n\r\n".data(using: .utf8) {
+            body.append(data1)
+        }
+        if let parameter = parameters?[key], let data1 = "\(parameter)\r\n".data(using: .utf8) {
+            body.append(data1)
+        }
+    }
+    
+    if let data1 = "--\(boundary ?? "")--\r\n".data(using: .utf8) {
+        body.append(data1)
+    }
+    
+    return body
+}
